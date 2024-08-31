f79efd3f427f5be53b8fb37ff03ec2fd2bb4e272
JulieJulieMary <rotaru.iulia.maria@gmail.com>
2024-08-31 19:07:07 +0300

    ngrok

--- frontend/js/tryityourself.js
+++ frontend/js/tryityourself.js
@@ -33,7 +33,7 @@ function captureImage() {
         formData.append('file', blob, 'captured.png');
 
         // Send the image to the Flask backend using fetch
-        fetch('https://6fc1-79-118-187-217.ngrok-free.app/upload', {  // Use your updated ngrok URL
+        fetch('https://33c7-79-118-187-217.ngrok-free.app/upload', {  // Use your updated ngrok URL
             method: 'POST',
             body: formData
         })
